4884310526f8d9f81ce906499611a2b6bbeefcea
Eamonn O'Brien-Strain <eobrain@gmail.com>
2023-11-22 18:19:45 -0800

    Interesting floating point patterns

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const sleep = (delayMs) => new Promise((resolve) => setTimeout(resolve, delayMs)
 const ctx = $canvas.getContext('2d')
 
 ctx.fillStyle = 'black'
-ctx.fillRect(0, 0, 300, 300)
+ctx.fillRect(0, 0, 600, 600)
 
 const { width, height } = $canvas
 
@@ -29,20 +29,23 @@ function setValues (f) {
       const xp1 = (x + 1) % width
       const yp1 = (y + 1) % width
       const neighbors =
-        cells[prev][xm1][ym1] +
-        cells[prev][xm1][y] +
-        cells[prev][xm1][yp1] +
-        cells[prev][x][ym1] +
-        cells[prev][x][yp1] +
-        cells[prev][xp1][ym1] +
-        cells[prev][xp1][y] +
-        cells[prev][xp1][yp1]
-      cells[current][x][y] = f(neighbors, cells[prev][x][y])
+                cells[prev][xm1][ym1] +
+                cells[prev][xm1][y] +
+                cells[prev][xm1][yp1] +
+                cells[prev][x][ym1] +
+                cells[prev][x][yp1] +
+                cells[prev][xp1][ym1] +
+                cells[prev][xp1][y] +
+                cells[prev][xp1][yp1]
+      const dx = x - width / 2
+      const dy = y - height / 2
+      const r = Math.sqrt(dx * dx + dy * dy)
+      cells[current][x][y] = f(neighbors, cells[prev][x][y], r)
     }
   }
 }
 
-setValues(() => Math.random() > 0.3)
+setValues((neighbors, cell, r) => r > width / 4 ? 0 : (Math.random() > 0.5))
 
 const setPixels = () => {
   const imageData = ctx.getImageData(0, 0, width, height)
@@ -51,20 +54,31 @@ const setPixels = () => {
     const x = Math.trunc((i / 4) / width)
     const y = (i / 4) % height
     const value = cells[current][x][y]
-    data[i] = 255 * value // red
-    data[i + 1] = 255 * value // green
-    data[i + 2] = 255 * value // blue
+    const pix = Math.min(255, Math.trunc(256 * value))
+    data[i] = pix // red
+    data[i + 1] = pix // green
+    data[i + 2] = pix // blue
   }
   ctx.putImageData(imageData, 0, 0)
 }
 
 setPixels()
 
+const g = p0 => p => Math.exp(-((p - p0) * (p - p0)))
+
+const g2 = g(2)
+const g3 = g(3)
+
 async function run () {
   for (let t = 0; t < 1000000; ++t) {
     current = 0 + !current
     await sleep(0)
-    setValues((neighbors, cell) => neighbors === 3 || (neighbors === 2 && cell))
+    // setValues((neighbors, cell) => neighbors === 3 || (neighbors === 2 && cell))
+    // setValues((neighbors, cell) => Math.exp(-(((neighbors - 3 + cell / 2) ** 2) / 0.559)))
+    setValues((neighbors, cell) =>
+      g3(neighbors) + cell * g2(neighbors) * (1.0 / 1.36787944117144)
+    )
+
     setPixels()
   }
 }
